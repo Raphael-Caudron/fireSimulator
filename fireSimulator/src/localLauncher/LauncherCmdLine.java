6ef041285ed4fcf163961f8ef804ebc4d5a099f4
Raphael <raphael0450@gmail.com>
2023-06-26 18:52:17 +0200

    lecture de fichier de configuration fait

--- fireSimulator/src/localLauncher/LauncherCmdLine.java
+++ fireSimulator/src/localLauncher/LauncherCmdLine.java
@@ -1,23 +1,46 @@
 package localLauncher;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.Properties;
 
 import model.Grid;
 
 public class LauncherCmdLine {
 	
 	public static void main(String[] args) throws InterruptedException {
-		Grid myGrid = new Grid(8,8,4,50);
+		int hauteur;
+		int largeur;
+		int nbrFeu;
+		int proba;
+		
+		try (InputStream input = new FileInputStream("src/ressources/application.properties")) {
+		    Properties prop = new Properties();
+		    prop.load(input);
+		    hauteur = Integer.parseInt(prop.getProperty("hauteur"));
+		    largeur = Integer.parseInt(prop.getProperty("largeur"));
+		    nbrFeu = Integer.parseInt(prop.getProperty("nbrFeu"));
+		    proba = Integer.parseInt(prop.getProperty("probabilite"));
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			hauteur = 8;
+			largeur = 8;
+			nbrFeu = 3;
+			proba = 50;
+					
+		}
+		
+		Grid myGrid = new Grid(hauteur,largeur,nbrFeu,proba);
 		Boolean test = true;
 		while(test) {
 			List<String> grid = myGrid.getGridDisplay();
 			test = myGrid.getIsFire();
-			int haut = myGrid.getHauteur();
-			int larg = myGrid.getLargeur();
-			for(int i = 0;i<haut;i++){//Change
-				for(int j= 0;j<larg;j++) {
-					System.out.print(grid.get(i*larg+j));
+			for(int i = 0;i<hauteur;i++){//Change
+				for(int j= 0;j<largeur;j++) {
+					System.out.print(grid.get(i*largeur+j));
 				}
 				System.out.println();
 			}
